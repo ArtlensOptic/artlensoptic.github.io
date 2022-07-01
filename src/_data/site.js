ec96d4f156372ad866511ab963aafd75792daa9e
Sava <artlensoptic@gmail.com>
2022-07-01 19:18:25 +0300

    Initial commit

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,6 +3,6 @@ module.exports = {
       title: "Optica Medicala ArtlensOptic Braila",
       description: "Ochelari de vedere, ochelari de soare, accesorii, lentile de contact toate la Optica Medicala ArtlensOptic Braila",
       lang: "en",
-      siteUrl: "https://artlensoptic.ro/",
+      siteUrl: "https://artlensoptic.ro",
     }
   }
