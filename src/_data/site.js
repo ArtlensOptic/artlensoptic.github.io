1f386cfa503775cbd2fa18d9d04c2e026a4af144
Sava <artlensoptic@gmail.com>
2023-02-26 17:12:31 +0200

    Modified footer for better view on small devices

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,8 @@ module.exports = {
     meta: {
       title: "Optica Medicală ArtLensOptic Brăila",
       description: "Ochelari de vedere, ochelari de soare, accesorii, lentile de contact! Toate la Optica Medicală ArtlensOptic Brăila",
-      lang: "en",
+      lang: "ro",
+      business: "ArtLensOptic",
       siteUrl: "https://artlensoptic.ro",
     }
   }
