eaf466f4b943e0c57d74003954cbba73471c34bf
Sava <artlensoptic@gmail.com>
2024-11-07 16:40:04 +0200

    new html minify

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,22 @@
-const eleventyPluginFilesMinifier = require("@sherby/eleventy-plugin-files-minifier");
+const htmlmin = require("html-minifier-terser");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/static/");
     eleventyConfig.addPassthroughCopy("./src/robotx.txt");
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
-    eleventyConfig.addPlugin(eleventyPluginFilesMinifier);
+    eleventyConfig.addTransform("htmlmin", function (content) {
+		if ((this.page.outputPath || "").endsWith(".html")) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true,
+                collapseInlineTagWhitespace: true,
+                preserveLineBreaks: true
+			});
+			return minified;
+		}
+		return content;
+	});
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
     return {
         dir: {
